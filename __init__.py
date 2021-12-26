e28083060ec60fc8006fd23e3c2b9756f70a1f33
BlackGameOn <67538261+BlackGameOn@users.noreply.github.com>
2021-12-26 23:29:21 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,19 @@
+from mycroft import MycroftSkill, intent_handler
+import RPi.GPIO as GPIO
+
+GPIO.setup(7,GPIO.OUT)
+
+class LedControle(MycroftSkill):
+    
+    def __init__(self):
+        MycroftSkill.__init__(self)
+        
+
+    @intent_handler('led_skill.intent')
+    def handle_led_skill(self, message):
+        self.speak_dialog('led_skill')
+        GPIO.output(7,False)
+
+def create_skill():
+    return LedControle()
+
