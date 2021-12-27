5c3e36b23d82f415124a1c3dc445e7bd77071110
BlackGameOn <67538261+BlackGameOn@users.noreply.github.com>
2021-12-27 15:45:11 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,6 +2,8 @@ from mycroft import MycroftSkill, intent_handler
 import RPi.GPIO as GPIO
 
 GPIO.setup(14,GPIO.OUT)
+GPIO.setup(15,GPIO.OUT)
+GPIO.setup(18,GPIO.OUT)
 
 class LedControle(MycroftSkill):
     
@@ -13,6 +15,8 @@ class LedControle(MycroftSkill):
     def handle_led_skille(self, message):
         self.speak_dialog('led_skill')
         GPIO.output(14,False)
+        GPIO.output(15,False)
+        GPIO.output(18,False)
 
 def create_skill():
     return LedControle()
